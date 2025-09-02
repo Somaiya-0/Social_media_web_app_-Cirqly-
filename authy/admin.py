0ab947f1c4a0c524f9dcc64840ad71cb25135ce5
Somaiya-0 <sdark.babe@gmail.com>
2025-09-03 02:38:58 +0600

    Modified

--- authy/admin.py
+++ authy/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Profile, Post, Comment, Follow, Notification
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'bio', 'followers_count')
+    search_fields = ('user__username',)
+
+    def followers_count(self, obj):
+        return obj.followers.count()
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'content', 'created_at', 'total_likes')
+    search_fields = ('user__username', 'content')
+    list_filter = ('created_at',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'post', 'content', 'created_at')
+    search_fields = ('user__username', 'content')
+    list_filter = ('created_at',)
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('id', 'follower', 'following', 'created_at')
+    search_fields = ('follower__username', 'following__username')
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sender', 'recipient', 'notification_type', 'created_at', 'is_read')
+    list_filter = ('notification_type', 'is_read', 'created_at')
+    search_fields = ('sender__username', 'recipient__username')
